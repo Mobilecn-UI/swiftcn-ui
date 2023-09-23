23d23d5b707ad5dae9b20e746e01e796e00207aa
Suman Sigdel <sumansid1113@gmail.com>
2023-09-23 15:11:27 -0700

    Add dot pattern and shimmer btn

---
+++ Swiftcn Playground.swiftpm/Components/DotPattern.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+
+struct DotPattern: View {
+    var dotColor: Color = Color.blue
+    var spacing: CGFloat = 0
+    var brightness: Double = 1.0 // Range from 0.0 to 1.0
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                let xDots = Int(ceil(geometry.size.width / (16 + spacing)))
+                let yDots = Int(ceil(geometry.size.height / (16 + spacing)))
+                
+                ForEach(0..<xDots, id: \.self) { xIndex in
+                    ForEach(0..<yDots, id: \.self) { yIndex in
+                        Circle()
+                            .fill(dotColor)
+                            .opacity(brightness)
+                            .frame(width: 2, height: 2)
+                            .offset(
+                                x: CGFloat(xIndex) * (16 + spacing) + 1,
+                                y: CGFloat(yIndex) * (16 + spacing) + 1
+                            )
+                    }
+                }
+            }
+        }
+        .background(Color.black)
+        .ignoresSafeArea()
+    }
+}
+
+struct DotPattern_Previews: PreviewProvider {
+    static var previews: some View {
+        DotPattern(dotColor: Color.white, spacing: 15, brightness: 0.5)
+    }
+}
